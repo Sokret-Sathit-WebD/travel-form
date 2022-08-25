f6d86bccd7c6e3f32756eb13d95e819764efbedf
sokret sathit <sokretsathit13@gmail.com>
2022-08-25 15:35:48 -0500

    Travel form created

---
+++ app.js
@@ -0,0 +1,13 @@
+const gender = document.gender
+
+gender.addEventListener("submit", (event) => {
+    event.preventDefault()
+    console.log(gender.gender.value)
+})
+
+const from = document.destination
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    console.log(form.destination.value)
+})
